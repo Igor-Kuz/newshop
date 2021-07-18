df936d9ee654a583dc20230ee636a64d678f3561
Igor-Kuz <dragonusty@gmail.com>
2021-07-19 01:13:51 +0300

    initial commit

---
+++ mainapp/templatetags/search_filter.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+from django import template
+from django.utils.safestring import mark_safe
+
+# from specs.models import ProductFeatures
+
+#register = template.Library()
+
+#@register.filter
